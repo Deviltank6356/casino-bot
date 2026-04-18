a7ffee88298c8400ecd27fd84ebda7baeb8b0c0b
deviltank6356 <granny40756@gmail.com>
2026-04-18 21:13:02 +0100

    fxed depost

--- commands/economy/deposit.js
+++ commands/economy/deposit.js
@@ -4,13 +4,23 @@ const { deposit } = require("../../systems/bank");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("deposit")
-    .addIntegerOption(o => o.setName("amount").setRequired(true))
-    .setDescription("Deposit money"),
+    .setDescription("Deposit money into your bank")
+    .addIntegerOption(o =>
+      o
+        .setName("amount")
+        .setDescription("Amount of money to deposit")
+        .setRequired(true)
+    ),
 
   async execute(i) {
-    const r = deposit(i.user.id, i.options.getInteger("amount"));
-    if (r.error) return i.reply(r.error);
+    const amount = i.options.getInteger("amount");
 
-    i.reply("🏦 Deposited");
+    const result = deposit(i.user.id, amount);
+
+    if (result?.error) {
+      return i.reply({ content: result.error, ephemeral: true });
+    }
+
+    return i.reply("🏦 Deposited successfully");
   }
 };
