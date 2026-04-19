2ec725d6af4192acc7651f9b66e2ec15a28ac935
deviltank6356 <granny40756@gmail.com>
2026-04-19 15:41:02 +0100

    fixed spotify intergration not working

--- commands/utils/Login.js
+++ commands/utils/Login.js
@@ -3,33 +3,15 @@ const config = require("../../config.json");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("spotify")
-    .setDescription("Spotify commands")
-    .addSubcommand(sub =>
-      sub
-        .setName("login")
-        .setDescription("Link your Spotify account")
-    ),
+    .setName("login")
+    .setDescription("Link your Spotify account"),
 
   async execute(i) {
-    const sub = i.options.getSubcommand();
-
-    // =============================
-    // LOGIN SUBCOMMAND
-    // =============================
-    if (sub === "login") {
-      const url =
-        `${config.spotify.authUrl}/login?user=${i.user.id}`;
-
-      return i.reply({
-        content: `🔗 Click to link Spotify:\n${url}`,
-        flags: 64 // replaces deprecated ephemeral
-      });
-    }
+    const url = `${config.spotify.authUrl}/login?user=${i.user.id}`;
 
     return i.reply({
-      content: "❌ Unknown subcommand",
-      flags: 64
+      content: `🔗 Click to link Spotify:\n${url}`,
+      flags: 64 // ephemeral replacement
     });
   }
 };
